28ed340ecfc419ce07b405e5e841b9f7f79f5b56
Vashishth Raval <vraval765@gmail.com>
2026-01-16 22:42:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Chrome plugin to empower influencers by providing in-depth analysis of their YouTube video comments, helping them make data-driven decisions to enhance their content and engagement strategies.',
+    author='Vashishth Raval',
+    license='',
+)
